3a9b8818321696f9414bae01662378529e94e095
Evan West <evan.west@cmrsurgical.com>
2020-12-16 14:53:16 +0000

    cors changes

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+//const cors = require("cors");
 
 const app = express();
 app.use(express.static("public"))
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
+// var corsOptions = {
+//   origin: "http://localhost:8081"
+// };
 
-app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
