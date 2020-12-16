deecb4a9ed1220375939504c0255053d5b12cc56
Evan West <evan.west@cmrsurgical.com>
2020-12-16 16:08:15 +0000

    cors settings

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.static("public"))
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*")
+  res.header("Access-Control-Allow-Methods", "*")
   next();
 });
 
